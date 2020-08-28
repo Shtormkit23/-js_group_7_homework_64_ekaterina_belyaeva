96b65ec52383d5efac887a640c781a6f15be877f
Belyaeva Ekaterina <katrinamalinashtorm@gmail.com>
2020-08-29 03:50:02 +0600

    Добавлен компонент Footer.js и добавлены стили к нему Footer.css

---
+++ src/component/Footer/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => (
+    <>
+        <footer className="footer">
+            <div className="footer-inner">
+                <div className="footer-logo">
+                    <img src="/BlogImg/logo .png" alt="logo" className="footer-logo-img"/>
+                    <p>Blog</p>
+                </div>
+            </div>
+        </footer>
+    </>
+);
+
+export default Footer;
